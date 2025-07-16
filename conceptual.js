dbd3807ab86853688b73caaf18d924c0cb478c9b
Bakir Bhuyain <hemalbakir@gmail.com>
2025-07-17 00:35:31 +0600

    Update conceptual.js with modulus examples

---
+++ conceptual.js
@@ -0,0 +1,18 @@
+//naming of  variable
+//j keyword gula js reserve kore rakhche she gula use korte parbo na 
+//Example:
+
+//modulas sikha;
+// var money = 23;
+// console.log(money%2);
+// var age =26;
+// console.log(age%2);
+
+
+// konno voro sonkha ke choto sonkha diye vag krle kkhn ow ei result ashbe na result er jaygay chto sonkha kei dekhabe
+// kono sonkha k jodi chot kono sokhna diye modulas  kori tahole kkhn owi oi choto sokhnar behsi ashe na jmn dhoren 22 k amrA 4 diye modulas korechi
+var money = 22;
+console.log(money%4);
+
+var age = 4;
+console.log(age%7);
