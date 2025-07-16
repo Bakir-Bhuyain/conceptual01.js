921e62b3d49e9dc41798f55809e32b2727797e39
Bakir Bhuyain <hemalbakir@gmail.com>
2025-07-17 00:35:20 +0600

    Update shorthand.js with shorthand operator examples

---
+++ shorthand.js
@@ -0,0 +1,15 @@
+// oldest system
+var x = 5;
+// x = x+5;
+// console.log(x)
+//shorthand system;
+x += 5;
+// console.log(x);
+x -= 5;
+// console.log(x);
+
+x *= 5;
+// console.log(x);
+
+x /= 2;
+console.log(x);
