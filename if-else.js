2ed2cf68e5bebef6a5db694c931b41dcfcbc14dc
Bakir Bhuyain <hemalbakir@gmail.com>
2025-07-17 00:34:55 +0600

    Update if-else.js logical OR example

---
+++ if-else.js
@@ -0,0 +1,88 @@
+// jodi x er man 10 er ceye choto hoy tahole ashbe ekta output ,jodi x er man 10 er ceye bor hoy tahole ashbe j tmi sherra;
+// var x = 5;
+// if (x<10){
+//       console.log('you are brave');
+// }else{
+//       console.log('go away');
+// }
+
+// const v = 89;
+// if (v<54 && v>60){
+//       console.log('bro is boro manush');
+// }else{
+//       console.log('choto manush');
+// }
+
+// var abc = 40;
+// if(abc === 30){
+//       console.log('equal to 30')
+// }else{
+//       console.log('no boro')
+// }
+// var abc = 40;
+// if(abc !== 30){
+//       console.log('equal to 30')
+// }else{
+//       console.log('no boro')
+// }
+
+// var temperature = 30;
+// if (temperature > 30 ){
+//       console.log("its's a hot day");
+// }else if(temperature>20){
+//       console.log("it's a warm day")
+// }else if(temperature>10){
+//       console.log("it's a cool day");
+// }else{
+//       console.log("its's a cold day");
+// }
+
+/* bejhailla syntex [nested if else] */
+
+// var hour = 14;
+// if (hour >= 6 && hour > 12){
+//       console.log("Good Morning");
+// }else{
+//       if((hour >= 12 && hour <18)){
+//             console.log("Good Afternoon");
+
+//       }else{
+//             console.log("Good Evening");
+//       }
+// }
+
+// var hour = 14;
+// if (hour >= 6 && hour >12){
+//       console.log("Good morning");
+// }else {
+//       if (hour >= 12 && hour > 18){
+//             console.log("Good Evening");
+//       }else{
+//             console.log("Good evening");
+//       }
+// }
+
+// var hour = 14;
+// if (hour >= 6 && hour > 12){
+
+//       console.log (" Good Morning");
+// }else {
+//       if (hour >= 12 && hour > 18){
+//             console.log("Good Afternoon");
+//       }else{
+//             console.log("Good Evening");
+//       }
+// }
+
+
+/* Logical Or */
+
+// var x = 5;
+
+// // x er man jodi 5 er choto hoy & 10 er boro hoy tahole true hbe ;
+
+// if (x > 5 || x > 10){
+//       console.log('true');
+// }else {
+//       console.log("false");
+// }
