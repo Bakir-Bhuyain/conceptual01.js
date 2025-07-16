25b94a5b99510be75dcc079207984f21c8a05621
Bakir Bhuyain <hemalbakir@gmail.com>
2025-07-17 00:34:40 +0600

    Update ternary.js with ternary and comparison examples

---
+++ ternary.js
@@ -0,0 +1,27 @@
+var x = 10;
+var message;
+
+// if (x>5 ){
+//       message= "Bigger then 5";
+// }else {
+//       message=  "less than 5";
+// }
+
+
+//ternary condition
+//condition ? if  : false 
+
+message = x > 5 ? "Bigger then 5" : "less then 5";
+// console.log(message);
+
+
+//ultate chaile!
+
+// var sunnyDay = true;
+// console.log(!sunnyDay);
+
+// console.log(!(100===1));
+
+// difference of == and ===
+/* == compare only value */
+/* === compares value with data type */
