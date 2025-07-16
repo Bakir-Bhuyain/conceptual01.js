c5686cb3a5f336cb0fdf65c890ce3eeacd32dff3
Bakir Bhuyain <hemalbakir@gmail.com>
2025-07-17 00:35:09 +0600

    Update convertion.js with isNaN examples and conversion notes

---
+++ convertion.js
@@ -0,0 +1,41 @@
+//means convert kora/ mane kono ekta form k onno ekta form e neya is called convertion
+
+// var number = 14.132;
+
+// var x = "19.234";
+// var y = "20";
+// // console.log(parseInt(number));
+
+// // console.log(parseInt(x));
+// // console.log(parseFloat(x));
+
+// var num = parseFloat(x);
+// console.log(num);
+
+// var z = parseFloat(y);
+// console.log(z);
+
+
+// const a = "20";
+// console.log(typeof a);
+// var converted = parseInt(a);
+// console.log(typeof converted);
+
+// console.log(3 + "3");
+// console.log(true + 3);
+
+//toFixed use korle data type change hoye jay jmn number type thakle string hoye jay; tai amra caile etar sathe  jog biyog korte parbo na
+// var x = 4.578434;
+// console.log(typeof x);
+// const convert = x.toFixed(3);
+// console.log( 5 + convert);
+// console.log(typeof convert);
+
+// console.log(5 + parseFloat(convert));
+
+/* isNaN jodi number hoy ba number a convert kora psbl hoy tahole false pathabe---noyoto false dibe mane jetake number e convert kora psbl na hoy tahole true dibe */
+
+console.log(isNaN(8));
+console.log(isNaN('23'));
+console.log(isNaN('bakkir'));
+//isNaN 
